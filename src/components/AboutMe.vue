8cd01e72373318b36fdcb2e06049569fd41a697f
Adil Bourkia <adil.45b@gmail.com>
2024-03-21 02:16:14 +0100

    Ajout et personalisation du component AboutMe/mains.css/Header/index.html

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="AboutMe">
+    <h2>Qui suis-je ?</h2>
+    <p>Je m'appel Adil Bourkia, j'ai 25ans et je suis un passionné de technologies et de développement Web, toujours avide d'apprendre et de relever de nouveaux défis. Avec une solide base en HTML, CSS et JavaScript, je suis motivé pour mettre mes compétences au service de projets innovants et captivants. <br> Mon approche méthodique, ma capacité à résoudre des problèmes et ma volonté d'amélioration constante font de moi un développeur Web Junior déterminé à exceller dans ce domaine en pleine évolution !</p>
+</div>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style scope>
+#AboutMe {
+    display: flex;
+    flex-direction: column;
+    width: 40%;
+    margin: 0 auto;
+    text-align: center;
+    padding: 20px 0;
+    
+    scroll-margin-top: 50px;
+    background-color: rgba(250, 235, 215, 0.679);
+    border-radius: 30%;  
+}
+
+
+</style>
